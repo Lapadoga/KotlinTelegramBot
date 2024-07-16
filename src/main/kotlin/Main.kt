ba1ebd43a50b76ffffa266844047031925099323
zhenya <ev.petrov@icloud.com>
2024-07-16 13:18:30 +0300

    KTB-04 спарсил файл словаря

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,17 @@ import java.io.File
 
 fun main() {
     val file = File("words.txt")
+    val listOfWords = mutableListOf<Word>()
 
-    val lines = file.readLines()
-    lines.forEach {
+    val fileLines = file.readLines()
+    fileLines.forEach {
+        val lineParts = it.split("|")
+        val correctAnswers = if (lineParts[2] == "") 0 else lineParts[2].toInt()
+        val word = Word(original = lineParts[0], translate = lineParts[1], correctAnswers)
+        listOfWords.add(word)
+    }
+
+    listOfWords.forEach {
         println(it)
     }
 }
