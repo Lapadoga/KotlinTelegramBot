9bd0689fe1bed284d11667f7e4799fe0fa2cc120
zhenya <ev.petrov@icloud.com>
2024-07-16 13:28:26 +0300

    KTB-04 добавил обработку исключения выхода за пределы массива

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,7 +7,12 @@ fun main() {
     val fileLines = file.readLines()
     fileLines.forEach {
         val lineParts = it.split("|")
-        val correctAnswers = if (lineParts[2] == "") 0 else lineParts[2].toInt()
+        val correctAnswers = try {
+            if (lineParts[2] == "") 0 else lineParts[2].toInt()
+        } catch (e: IndexOutOfBoundsException) {
+            0
+        }
+
         val word = Word(original = lineParts[0], translate = lineParts[1], correctAnswers)
         listOfWords.add(word)
     }
