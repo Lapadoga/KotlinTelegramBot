147904ced86f2e6961771ded26192562b5f39a02
zhenya <ev.petrov@icloud.com>
2024-07-29 22:40:22 +0300

    KTB-07 реализовал функционал изучения слов

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ import java.io.File
 
 const val MIN_CORRECT_ANSWERS = 3
 const val LAST_MENU_ELEMENT = 2
+const val TEST_WORDS_COUNT = 4
 
 fun main() {
     val listOfWord = parseFile("words.txt")
@@ -23,21 +24,22 @@ fun main() {
         }
 
         when (userAnswer) {
-            1 -> println("Учить слова")
-            2 -> {
-                val listOfLearnedWord = listOfWord.filter { it.correctAnswersCount >= MIN_CORRECT_ANSWERS }
-                val percentString = String.format("%.0f", listOfLearnedWord.size.toFloat() / listOfWord.size * 100)
-                println(
-                    "Выучено ${listOfLearnedWord.size} из ${listOfWord.size} слов | ${percentString}%"
-                )
+            1 -> {
+                val wordsLearned = startLearning(listOfWord)
+                if (wordsLearned) {
+                    println("Вы выучили все слова")
+                    return
+                }
             }
 
+            2 -> println(statisticsString(listOfWord))
             0 -> break
         }
     }
 }
 
 fun parseFile(path: String): List<Word> {
+
     val file = File(path)
     val listOfWords = mutableListOf<Word>()
 
@@ -54,3 +56,59 @@ fun parseFile(path: String): List<Word> {
     }
     return listOfWords
 }
+
+fun statisticsString(listOfWord: List<Word>): String {
+
+    val listOfLearnedWord = listOfWord.filter { it.correctAnswersCount >= MIN_CORRECT_ANSWERS }
+    val percentString = String.format("%.0f", listOfLearnedWord.size.toFloat() / listOfWord.size * 100)
+
+    return "Выучено ${listOfLearnedWord.size} из ${listOfWord.size} слов | ${percentString}%"
+}
+
+fun startLearning(listOfWords: List<Word>): Boolean {
+
+    do {
+
+        val listOfUnlearnedWords = listOfWords.filter { it.correctAnswersCount < MIN_CORRECT_ANSWERS }
+        if (listOfUnlearnedWords.isEmpty())
+            return true
+
+        val shuffledList = listOfUnlearnedWords.shuffled()
+        var testList = shuffledList.take(TEST_WORDS_COUNT)
+
+        val testWord = testList[0]
+        println(testWord.original)
+        testList = testList.shuffled()
+        println(
+            """            
+            -----------------------
+            1. ${testList[0].translate}
+            2. ${testList.getOrNull(1)?.translate ?: "---"}
+            3. ${testList.getOrNull(2)?.translate ?: "---"}
+            4. ${testList.getOrNull(3)?.translate ?: "---"}
+            0. Меню
+        """.trimIndent()
+        )
+
+        var userAnswer = readln().toIntOrNull()
+        while (true) {
+
+            if (userAnswer == null || userAnswer !in 0..TEST_WORDS_COUNT) {
+                println("Неверный ввод, повторите попытку")
+                userAnswer = readln().toIntOrNull()
+            } else
+                break
+        }
+
+        if (userAnswer != 0)
+            if (testList.getOrNull(userAnswer!! - 1) == testWord)
+                println("Верно!")
+            else
+                println("Неверно")
+
+    } while (userAnswer != 0)
+
+    return false
+}
+
+
