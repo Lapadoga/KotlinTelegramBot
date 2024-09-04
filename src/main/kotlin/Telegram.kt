fa2919ebe66e2240963e9e9b88f8c3c2e3db3272
zhenya <zhenya1032@gmail.com>
2024-09-04 22:47:49 +0300

    KTB-16 реализовал отправку кнопок меню в телеграмм бот

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -4,29 +4,50 @@ fun main(args: Array<String>) {
     var updateId = 0
     val telegramService = TelegramBotService(botToken)
 
+    val dataRegex = "\"data\":\"(.+)\"".toRegex()
+    val textMessageRegex = "\"text\":\"(.+)\"".toRegex()
+    val chatIdRegex = "\"chat\":\\{\"id\":(\\d+)".toRegex()
+    val messageIdRegex = "\"update_id\":(\\d+)".toRegex()
+
     while (true) {
         Thread.sleep(2000)
         val updates = telegramService.getUpdates(updateId)
         println(updates)
 
-        val messageIdRegex = "\"update_id\":(\\d+)".toRegex()
         val updateIdString = telegramService.parseResponse(messageIdRegex, updates)
         if (updateIdString != null)
             updateId = updateIdString.toInt() + 1
 
-        val chatIdRegex = "\"chat\":\\{\"id\":(\\d+)".toRegex()
         val chatIdString = telegramService.parseResponse(chatIdRegex, updates)
-
-        val textMessageRegex = "\"text\":\"(.+)\"".toRegex()
         val text = telegramService.parseResponse(textMessageRegex, updates)
+        val data = telegramService.parseResponse(dataRegex, updates)
 
         if (chatIdString != null && text != null) {
-            try {
-                telegramService.sendMessage(chatIdString, text)
-            } catch (e: Exception) {
-                println(e.message)
-            }
+            if (text.equals("menu", ignoreCase = true))
+                try {
+                    telegramService.sendMenu(chatIdString)
+                } catch (e: Exception) {
+                    println(e.message)
+                }
+        }
+        if (chatIdString != null && data != null) {
+            when (data.lowercase()) {
+                "learn_words_clicked" -> {
+                    try {
+                        telegramService.sendMessage(chatIdString, "Изучение слов")
+                    } catch (e: Exception) {
+                        println(e.message)
+                    }
+                }
 
+                "statistics_clicked" -> {
+                    try {
+                        telegramService.sendMessage(chatIdString, "Выучено 10 из 10 слов | 100%")
+                    } catch (e: Exception) {
+                        println(e.message)
+                    }
+                }
+            }
         }
     }
 
--- src/main/kotlin/TelegramBotService.kt
+++ src/main/kotlin/TelegramBotService.kt
@@ -1,6 +1,8 @@
 import java.net.URI
+import java.net.URLEncoder
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
+import java.net.http.HttpRequest.BodyPublishers
 import java.net.http.HttpResponse
 
 const val HOST = "https://api.telegram.org/"
@@ -21,16 +23,51 @@ class TelegramBotService(botToken: String) {
     }
 
     fun sendMessage(chatId: String, messageText: String) {
-        val urlSendMessage = "${hostWithToken}sendMessage?chat_id=$chatId&text=$messageText"
+        val encodedMessage = URLEncoder.encode(messageText, Charsets.UTF_8)
+        val urlSendMessage = "${hostWithToken}sendMessage?chat_id=$chatId&text=$encodedMessage"
         val client = HttpClient.newBuilder().build()
         val request = HttpRequest.newBuilder(URI(urlSendMessage)).build()
         client.send(request, HttpResponse.BodyHandlers.ofString())
     }
 
+    fun sendMenu(chatId: String) {
+        val urlSendMessage = "${hostWithToken}sendMessage"
+        val menuBody = """
+            {
+            	"chat_id": $chatId,
+            	"text": "Основное меню",
+            	"reply_markup": {
+            		"inline_keyboard": [
+            			[
+            				{
+            					"text": "Изучить слова",
+            					"callback_data": "learn_words_clicked"
+            				}
+            			],
+            			[
+            				{
+            					"text": "Статистика",
+            					"callback_data": "statistics_clicked"
+            				}
+            			]
+            		]
+            	}
+            }
+        """.trimIndent()
+
+        val client = HttpClient.newBuilder().build()
+        val request = HttpRequest.newBuilder(URI(urlSendMessage))
+            .header("Content-type", "application/json")
+            .POST(BodyPublishers.ofString(menuBody))
+            .build()
+        client.send(request, HttpResponse.BodyHandlers.ofString())
+    }
+
     fun parseResponse(regex: Regex, response: String): String? {
         val matchChatIdResult = regex.find(response)
+        val result = matchChatIdResult?.groups?.get(1)?.value
 
-        return matchChatIdResult?.groups?.get(1)?.value
+        return result
     }
 
 }
